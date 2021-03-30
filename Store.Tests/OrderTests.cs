7635458a1105b57e8fc7d8c960c0bbf5b6463aba
Kirillkik <78596284+Kirillkik@users.noreply.github.com>
2021-03-30 22:42:03 +0500

    Refactor orders

--- Store.Tests/OrderTests.cs
+++ Store.Tests/OrderTests.cs
@@ -48,5 +48,90 @@ namespace Store.Tests
             });
             Assert.Equal(3 * 10m + 5 * 200m, order.TotalPrice);
         }
+
+        [Fact]
+        public void GetItem_WithExistingItem_ReturnsItem()
+        {
+            var order = new Order(0, new[]
+            {
+                new OrderItem(0, 3, 10m),
+                new OrderItem(1, 5, 200m),
+            });
+
+            var orderItem = order.GetItem(1);
+
+            Assert.Equal(5, orderItem.Count);
+        }
+
+        [Fact]
+        public void GetItem_WithNonExistingItem_ThrowInvalidOperationException()
+        {
+            var order = new Order(0, new[]
+            {
+                new OrderItem(0, 3, 10m),
+                new OrderItem(1, 5, 200m),
+            });
+
+            Assert.Throws<InvalidOperationException>(() => order.GetItem(100));
+        }
+
+        [Fact]
+        public void AddOrUpdateItem_WithExistingItem_UpdateCount()
+        {
+            var order = new Order(0, new[]
+            {
+                new OrderItem(0, 3, 10m),
+                new OrderItem(1, 5, 200m),
+            });
+
+            var book = new Book(1, null, null, null, null, 0m);
+
+            order.AddOrUpdateItem(book, 10);
+
+            Assert.Equal(5 + 10, order.GetItem(1).Count);
+        }
+
+        [Fact]
+        public void AddOrUpdateItem_WithNonExistingItem_AddCount()
+        {
+            var order = new Order(0, new[]
+            {
+                new OrderItem(0, 3, 10m),
+                new OrderItem(1, 5, 200m),
+            });
+
+            var book = new Book(5, null, null, null, null, 0m);
+
+            order.AddOrUpdateItem(book, 10);
+
+            Assert.Equal(10, order.GetItem(5).Count);
+        }
+
+        [Fact]
+        public void RemoveItem_WithExistingItem_RemovesItem()
+        {
+            var order = new Order(0, new[]
+            {
+                new OrderItem(0, 3, 10m),
+                new OrderItem(1, 5, 200m),
+            });
+
+            order.RemoveItem(1);
+
+            Assert.Equal(1, order.Items.Count);
+        }
+
+        [Fact]
+        public void RemoveItem_WithNonExistingItem_ThrowInvalidOperationException()
+        {
+            var order = new Order(0, new[]
+            {
+                new OrderItem(0, 3, 10m),
+                new OrderItem(1, 5, 200m),
+            });
+
+            Assert.Throws<InvalidOperationException>(() => order.RemoveItem(5));
+        }
+
     }
 }
--- Store.Tests/OrederItemTests.cs
+++ Store.Tests/OrederItemTests.cs
@@ -26,12 +26,34 @@ namespace Store.Tests
         }
 
         [Fact]
-        public void OrderItem_WithPositiveCount_TrowsArgumentOutOfRangeException()
+        public void OrderItem_WithPositiveCount_SetsCount()
         {
             var orderitem = new OrderItem(2, 10, 5m);
             Assert.Equal(2, orderitem.BookId);
             Assert.Equal(10, orderitem.Count);
             Assert.Equal(5m, orderitem.Price);
+        }
+
+        [Fact]
+        public void Count_WithNegativeValue_ThrowArgumentOutOfRangeException()
+        {
+            var orderitem = new OrderItem(2, 10, 5m);
+            Assert.Throws<ArgumentOutOfRangeException>(() => orderitem.Count = -1);
+        }
+
+        [Fact]
+        public void Count_WithZeroValue_ThrowArgumentOutOfRangeException()
+        {
+            var orderitem = new OrderItem(2, 10, 5m);
+            Assert.Throws<ArgumentOutOfRangeException>(() => orderitem.Count = -1);
+        }
+
+        [Fact]
+        public void Count_WithPositiveValue_SetsCount()
+        {
+            var orderitem = new OrderItem(2, 10, 5m);
+            orderitem.Count = 5;
+            Assert.Equal(5, orderitem.Count);
         }
     }
 }
